91bd808f20b20da2e4fd1e785a39d09ba535ec72
SUDIPP22 <panja370@gmail.com>
2021-08-19 03:35:25 +0530

    Added : Welcome Message

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose - To create a linked list
+ * @author SUDIP PANJA
+ * @version 11.0.11
+ * @since 2021-08-18
+ */
+public class LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Welcome To The Linked List Program");
+    }
+}
